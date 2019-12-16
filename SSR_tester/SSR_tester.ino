b88a294c4f53def0fbeeb0817b2d81fffe00b23e
Phatcharapon Jumruspun <pokemonlizardon@gmail.com>
2019-12-16 13:16:21 +0700

    Added forced manual feature in ESP8266 Code

--- SSR_tester/SSR_tester.ino
+++ SSR_tester/SSR_tester.ino
@@ -1,11 +1,10 @@
- #include <Arduino.h>
-
+#include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
 ESP8266WiFiMulti WiFiMulti;
 
 #define URL "http://08558270.ngrok.io/status_request"
@@ -26,6 +25,7 @@ int ignoringVariable = 0;
 float lastDustLevel = 0;
 
 int iter = 0;
+SoftwareSerial mySerial(D6, D5); // RX | TX
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,14 +35,14 @@ void setup() {
   pinMode(analogPin, INPUT);
   Serial.begin(115200); 
   for (uint8_t t = 4; t > 0; t--) {
-    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-    USE_SERIAL.flush();
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
   }
   digitalWrite(ledPin, HIGH);
   //digitalWrite(ssrPin, HIGH);
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("No free wifi for you uwu", "87654321");
+  WiFiMulti.addAP("SprkZ", "0000000000");
 }
 
 void loop() {
@@ -62,52 +62,61 @@ void loop() {
     //Serial.printf("Analog raw = %f, CalcVoltage = %f, DustDensity = %f\n", accumulatedValue, calcVoltage, dustDensity);
     accumulatedValue = 0;
   }
-  iter++;
   
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
-    USE_SERIAL.println("CONNECTED!!!");
+  
+  if ((WiFiMulti.run() == WL_CONNECTED) && iter%100 == 0) {
+    Serial.println("CONNECTED!!!");
     
     char token[10];
     HTTPClient http;
   
-    //USE_SERIAL.print("[HTTP] begin...\n");
+    //Serial.print("[HTTP] begin...\n");
     
     http.begin(URL); //HTTP
     //Serial.println(URL);
     
-    //USE_SERIAL.print("[HTTP] GET...\n");
+    //Serial.print("[HTTP] GET...\n");
     // start connection and send HTTP header
     int httpCode = http.GET();
-    //USE_SERIAL.printf("code: %d\n", httpCode);
+    //Serial.printf("code: %d\n", httpCode);
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      USE_SERIAL.printf("code: %d\n", httpCode);
+      Serial.printf("code: %d\n", httpCode);
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
         String useless = http.getString();
         char payload[50];
         useless.toCharArray(payload, 50);
-        USE_SERIAL.println(payload);
+        Serial.println(payload);
         processStatus(payload);
       
       } else {
-        USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
     http.end();
     }
   }
   //delay(500);
+  iter++;
 } 
 
 void processStatus(char* statusRequest){
   Serial.printf("Status processing: %s\n", statusRequest);
   if(strcmp(statusRequest, "on") == 0){
+    if(autoMode){
+      Serial.println("\tForcing manual mode...");
+      autoMode = 0;
+    }
      Serial.println("turning the device on!");
      digitalWrite(ssrPin, HIGH);
   }
   else if(strcmp(statusRequest, "off") == 0){
+    if(autoMode){
+      Serial.println("\tForcing manual mode...");
+      autoMode = 0;
+    }
     Serial.println("turning the device off!");
     digitalWrite(ssrPin, LOW);
   }
@@ -132,14 +141,16 @@ void processAutoMode(float dustLevel){
       Serial.println("turning the device on by auto mode!");
       ignoringVariable = MAX_IGN_VARIABLE;
   }
-  if(ignoringVariable > 0){
+  if(autoMode){
+    if(ignoringVariable > 0){
       digitalWrite(ssrPin, HIGH);
       Serial.println("\t still turned on...");
       ignoringVariable--;
-  }
-  else{
+    }
+    else{
       digitalWrite(ssrPin, LOW);
       Serial.println("System turned off by auto mode!");
       ignoringVariable = 0;
+    }
   }
 }
